cba55ba94c4c56666abaf4d610d96f3e7d88db9e
Jamesrolandv <vedanajamesroland@gmail.com>
2024-01-07 18:17:06 +0800

    send

--- send.php
+++ send.php
@@ -12,6 +12,15 @@ require 'phpmailer/src/SMTP.php';
 $mail = new PHPMailer(true);
 
 try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'vedanajamesroland@gmail.com';                     //SMTP username
+    $mail->Password   = 'gfkq coob lvwa gixm';                               //SMTP password
+    $mail->SMTPSecure = 'ssl';            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
     $mail->setFrom('vedanajamesroland@gmail.com');

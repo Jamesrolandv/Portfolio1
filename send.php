60d267137e5bc44cc8f4043831f6883765e5117d
Jamesrolandv <vedanajamesroland@gmail.com>
2024-01-07 18:55:33 +0800

    send

--- send.php
+++ send.php
@@ -8,9 +8,8 @@ require 'phpmailer/src/Exception.php';
 require 'phpmailer/src/PHPMailer.php';
 require 'phpmailer/src/SMTP.php';
 
-$mail = new PHPMailer(true);
-
-try {
+if(isset($_POST['send'])){
+    $mail = new PHPMailer(true);
     //Server settings
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
@@ -21,13 +20,13 @@ try {
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom($_POST['email']);
+    $mail->setFrom($_POST['email'], $_POST['name']);
     $mail->addAddress('vedanajamesroland@gmail.com');     //Add a recipient
 
     //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->isHTML(true);
     $mail->Subject = $_POST['name'];
-    $mail->Body    = $_POST['msg'];
+    $mail->Body    = $_POST['msg']; 
 
     $mail->send();
     echo '

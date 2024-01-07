3d1da3d203c193f15d6a00181a2ea0891363c84f
Jamesrolandv <vedanajamesroland@gmail.com>
2024-01-07 18:03:16 +0800

    send

--- send.php
+++ send.php
@@ -9,31 +9,6 @@ require 'phpmailer/src/Exception.php';
 require 'phpmailer/src/PHPMailer.php';
 require 'phpmailer/src/SMTP.php';
 
-// if(isset($_POST['send'])){
-
-
-//     $mail->isSMTP();
-//     $mail->Host = 'stmp.gmail.com';
-//     $mail->SMTPAuth = true;
-//     $mail->Username = 'vedanajamesroland@gmail.com';
-//     $mail->Password = 'gfkq coob lvwa gixm';
-//     $mail->SMTPSecure = 'ssl';
-//     $mail->Port = 465;
-//     $mail->setFrom('vedanajamesroland@gmail.com');
-//     $mail->addAddress($_POST['email']);
-//     $mail->isHTML(true);
-//     $mail->Subject = $_POST['name'];
-//     $mail->Body = $_POST['msg'];
-//     $mail->send();
-
-//     echo
-//     '
-//     <script>
-//         alert("Sent Succesfully");
-//         document.location.href = "index.html";
-//     </script>
-//     ';
-// }
 $mail = new PHPMailer(true);
 
 try {
